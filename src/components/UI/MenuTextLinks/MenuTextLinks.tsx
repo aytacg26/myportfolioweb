a1810b67e2699c7b72e278485fdb4cc2a7c0660f
aytacguley <aytacguley@dklassix.com>
2021-11-04 00:05:32 +0200

    TODO comment removed, required data file creeated for links

--- src/components/UI/MenuTextLinks/MenuTextLinks.tsx
+++ src/components/UI/MenuTextLinks/MenuTextLinks.tsx
@@ -2,7 +2,6 @@ import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import links from '../../../Data/MenuData';
 
-//create link data folder and use map
 const MenuTextLinks = () => {
   return (
     <Fragment>
